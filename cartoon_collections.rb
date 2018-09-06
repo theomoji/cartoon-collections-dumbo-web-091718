4ad1804438669280bd0cf769f9113b545e09cc32
Shuo Wang <gken90@gmail.com>
2018-09-06 23:22:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array_of_names)
 end
 
 def summon_captain_planet(array_of_planeteer_calls)
-  array_of_planeteer_calls.map {|element| element.capitalize + "!""}
+  array_of_planeteer_calls.map {|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls(array_of_calls)

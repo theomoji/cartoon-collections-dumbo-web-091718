a4813b29d5c60bc036971f9b82009a5b4d95e97e
Shuo Wang <gken90@gmail.com>
2018-09-06 23:33:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,11 +10,7 @@ end
 
 def long_planeteer_calls(array_of_calls)
   array_of_calls.each do |call|
-    if (call.length) > 4
-      return true
-    else
-      return false
-    end
+    (call.length) > 4?
   end
 end
 

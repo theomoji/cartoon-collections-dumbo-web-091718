c648c1ebcc284ff6eb76754224f2d8fcd398ece5
Shuo Wang <gken90@gmail.com>
2018-09-06 23:14:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,22 +16,3 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-
-
-
-# This method should accept an array argument of planeteer calls that will look like this:
-#
-# ```ruby
-# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-# ```
-#
-# It should then capitalize each element and add an exclamation point at the end. The return value of this method should be an array, in this example:
-#
-# ```
-# summon_captain_planet(planeteer_calls)
-# #=> ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-# ```
-#
-# The `.map` or `.collect` method might be appropriate for this task, take a look at it [here](http://stackoverflow.com/a/12084555/2890716) and [here](http://ruby-doc.org/core/Array.html#method-i-map).
-#
-# Once the test for this method is passing, move on to the next method, long planeteer calls.

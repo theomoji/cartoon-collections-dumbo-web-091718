9ffff03e9adaa5758f01968feeea103f34ebe7cb
Shuo Wang <gken90@gmail.com>
2018-09-07 00:31:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array_of_names)
+  array_of_names.each_with_index {|name, index|
+    puts "#{index + 1}. #{name}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array_of_planeteer_calls)
+  array_of_planeteer_calls.map {|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls)
+  array_of_calls.each do |call|
+    if call.length < 4
+      return true
+    else
+      return false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if array_of_strings.include?(cheese)
+      return cheese
+    end
+    return nil #return whatever you want if no cheese is found
+  end
 end
+
+#What I would do is loop over the array of cheeses you made, and use .includes? to check if the array_of_strings has an element. If it does, return that element
